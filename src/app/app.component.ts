e17ef226be5c2366de8c7cd63997e937abe8dee7
vinodkumarborse <vinod.borse@appdirect.com>
2020-08-19 14:33:50 +0530

    Add more user objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,8 @@ export class AppComponent {
   users: User[] = [
     new User(1, 'Vinu', 'Borse', 30, 'Admin'),
     new User(2, 'Vinod', 'Borse', 20, 'User'),
+    new User(3, 'Krish', 'Nikam', 17, 'User'),
+    new User(4, 'Pranjal', 'Patil', 5, 'Guest'),
+    new User(5, 'Tanmay', 'Nikam', 15, 'User')
   ];
 }
